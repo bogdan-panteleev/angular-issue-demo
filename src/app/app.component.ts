f0cd7890c145a45ab0761a76924cbc3125fe9556
Bogdan Panteleev <panteleevbbbogdan@gmail.com>
2021-04-09 16:44:55 +0300

    Reproduce differential loading issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lasttest';
+
+  myObservable = new Observable<number>((subscriber) => {
+    subscriber.next(1);
+    subscriber.next(2);
+  }).pipe(map((x) => x * 10));
 }
